243995e5708160d736fddce598d51c99c8094067
Jamova01 <jorgemova01@gmail.com>
2025-01-13 23:26:43 -0500

    Added navigation links data for website navigation

---
+++ src/data/navigationLinks.ts
@@ -0,0 +1,6 @@
+export const NAVIGATION_LINKS = [
+  { id: 1, label: "home", href: "/" },
+  { id: 2, label: "about", href: "#about" },
+  { id: 3, label: "projects", href: "#projects" },
+  { id: 4, label: "contact", href: "#contact" },
+];
