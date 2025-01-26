3614819f566faa57ba421d875eb46596626270f3
Jamova01 <jorgemova01@gmail.com>
2025-01-26 18:16:08 -0500

    Add technical skills data

---
+++ src/data/technologies.ts
@@ -0,0 +1,62 @@
+export const frontendTechData = [
+  {
+    id: 1,
+    name: "React",
+  },
+  {
+    id: 2,
+    name: "Node",
+  },
+
+  {
+    id: 3,
+    name: "HTML",
+  },
+  {
+    id: 4,
+    name: "CSS",
+  },
+  {
+    id: 5,
+    name: "Javascript",
+  },
+  {
+    id: 6,
+    name: "Sass",
+  },
+  {
+    id: 7,
+    name: "Tailwind CSS",
+  },
+];
+
+export const backendTechData = [
+  {
+    id: 1,
+    name: "Node",
+  },
+  {
+    id: 2,
+    name: "Python",
+  },
+  {
+    id: 3,
+    name: "FastAPI",
+  },
+  {
+    id: 4,
+    name: "Django",
+  },
+  {
+    id: 5,
+    name: "Django REST",
+  },
+  {
+    id: 6,
+    name: "MySQL",
+  },
+  {
+    id: 7,
+    name: "Postgres",
+  },
+];
