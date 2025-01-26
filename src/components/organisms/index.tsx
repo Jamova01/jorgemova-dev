533c337bcb1655df9e6b02cdf7d6fc84d13e05e5
Jamova01 <jorgemova01@gmail.com>
2025-01-26 17:14:33 -0500

    Export HeroSection, AboutMeSection, ContactSection, ProjectsSection, and SkillsSection for integration

--- src/components/organisms/index.tsx
+++ src/components/organisms/index.tsx
@@ -1,1 +1,5 @@
 export { HeroSection } from "./HeroSection";
+export { AboutMeSection } from "./AboutMeSection";
+export { ContactSection } from "./ContactSection";
+export { ProjectsSection } from "./ProjectsSection";
+export { SkillsSection } from "./SkillsSection/SkillsSection";
