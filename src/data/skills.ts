a051b994ed2321a9d7d7790fe320f196a6be87bf
Jamova01 <jorgemova01@gmail.com>
2025-01-26 18:15:32 -0500

    Add technical skills data

---
+++ src/data/skills.ts
@@ -0,0 +1,27 @@
+export const skillsData = [
+  {
+    id: 1,
+    name: "React",
+    icon: "<FaReact />",
+  },
+  {
+    id: 2,
+    name: "Node",
+    icon: "<FaReact />",
+  },
+  {
+    id: 3,
+    name: "Python",
+    icon: "<FaReact />",
+  },
+  {
+    id: 4,
+    name: "HTML",
+    icon: "<FaReact />",
+  },
+  {
+    id: 5,
+    name: "CSS",
+    icon: "<FaReact />",
+  },
+];
