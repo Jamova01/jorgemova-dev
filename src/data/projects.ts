d051b6626ab35b006f9cca82e9748d3b9f059a5b
Jamova01 <jorgemova01@gmail.com>
2025-01-26 19:35:36 -0500

    Add projects data

---
+++ src/data/projects.ts
@@ -0,0 +1,32 @@
+export const projectsData = [
+  {
+    id: 1,
+    image:
+      "https://images.pexels.com/photos/20701001/pexels-photo-20701001/free-photo-of-smart-devices-colorful-background-template.jpeg",
+    title: "Crypto Screener Application",
+    description: `I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and
+      typesetting industry. Lorem Ipsum has been the industry's standard
+      dummy text ever since the 1500s, when an unknown printer took a galley
+      of type and scrambled it to specimen book.`,
+  },
+  {
+    id: 2,
+    image:
+      "https://images.pexels.com/photos/20701001/pexels-photo-20701001/free-photo-of-smart-devices-colorful-background-template.jpeg",
+    title: "Crypto Screener Application",
+    description: `I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and
+      typesetting industry. Lorem Ipsum has been the industry's standard
+      dummy text ever since the 1500s, when an unknown printer took a galley
+      of type and scrambled it to specimen book.`,
+  },
+  {
+    id: 3,
+    image:
+      "https://images.pexels.com/photos/20701001/pexels-photo-20701001/free-photo-of-smart-devices-colorful-background-template.jpeg",
+    title: "Crypto Screener Application",
+    description: `I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and
+      typesetting industry. Lorem Ipsum has been the industry's standard
+      dummy text ever since the 1500s, when an unknown printer took a galley
+      of type and scrambled it to specimen book.`,
+  },
+];
