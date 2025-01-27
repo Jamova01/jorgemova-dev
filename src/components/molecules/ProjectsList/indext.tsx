b39b58e7a1aaee8b03270e7e85c478e9c681da94
Jamova01 <jorgemova01@gmail.com>
2025-01-26 19:45:46 -0500

    Add ProjectsList component for displaying project cards

---
+++ src/components/molecules/ProjectsList/indext.tsx
@@ -0,0 +1,22 @@
+import { Project } from "@/types/types";
+import { ProjectCard } from "../ProjectCard";
+
+interface Projects {
+  data: Project[];
+}
+
+export const ProjectsList: React.FC<Projects> = ({ data }) => {
+  return (
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto">
+      {data.map(({ id, image, title, description }) => (
+        <ProjectCard
+          key={id}
+          id={id}
+          image={image}
+          title={title}
+          description={description}
+        />
+      ))}
+    </div>
+  );
+};
