551006714dda98faa8841c66d131532167da393c
Jamova01 <jorgemova01@gmail.com>
2025-01-26 20:29:06 -0500

    add onItemClick to DropdownMenu for better interaction

--- src/components/molecules/DropdownMenu/index.tsx
+++ src/components/molecules/DropdownMenu/index.tsx
@@ -4,17 +4,19 @@ import { NavMenu } from "@/components/molecules";
 interface DropdownMenuProps {
   isOpen: boolean;
   items: NavigationLink[];
+  onItemClick: () => void;
 }
 
 export const DropdownMenu: React.FC<DropdownMenuProps> = ({
   isOpen,
   items,
+  onItemClick
 }) => (
   <div
     className={`absolute w-full top-20 right-0 left-0 bg-popover opacity-80 shadow-md rounded-lg transition-transform transform ${
       isOpen ? "scale-100 opacity-100" : "scale-0 opacity-0"
     } md:hidden`}
   >
-    <NavMenu items={items} className="p-4" orientation="vertical" />
+    <NavMenu onItemClick={onItemClick} items={items} className="p-4" orientation="vertical" />
   </div>
 );
