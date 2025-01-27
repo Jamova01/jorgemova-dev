463079616f0f0e996f302e21b32baa67cdb05821
Jamova01 <jorgemova01@gmail.com>
2025-01-26 20:13:31 -0500

    export reusable components for centralized access

--- src/components/molecules/index.tsx
+++ src/components/molecules/index.tsx
@@ -1,4 +1,7 @@
 export { Logo } from "./Logo";
+export { Tabs } from "./Tabs";
 export { Navbar } from "./Navbar";
 export { NavMenu } from "./NavMenu";
+export { Timeline } from "./Timeline";
 export { DropdownMenu } from "./DropdownMenu";
+export { SocialNetworks } from "./SocialNetworks";
