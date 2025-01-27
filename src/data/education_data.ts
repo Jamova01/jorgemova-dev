4fac22a4ea59d76f9acc76c897e2ebf5139f69fc
Jamova01 <jorgemova01@gmail.com>
2025-01-26 19:35:05 -0500

    Add education data

---
+++ src/data/education_data.ts
@@ -0,0 +1,26 @@
+export const educationData = [
+  {
+    id: 1,
+    title: "Platzi",
+    description: `Frontend con React.js`,
+    time: "06/2022 – 10/2023",
+  },
+  {
+    id: 2,
+    title: "Platzi",
+    description: `Full stack developer con JavaScript`,
+    time: "06/2022 – 06/2023",
+  },
+  {
+    id: 3,
+    title: "Academia Geek",
+    description: `Frontend development con énfasis en React.js`,
+    time: "03/2022 - 06/2022",
+  },
+  {
+    id: 4,
+    title: "Holberton School",
+    description: `Software Foundations`,
+    time: "01/2021 – 11/2021",
+  },
+];
