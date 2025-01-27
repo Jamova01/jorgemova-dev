5367d81f035a634df922f0ffa7fd912934e0ea24
Jamova01 <jorgemova01@gmail.com>
2025-01-26 20:23:26 -0500

    Implemented navbar hide/show behavior based on scroll direction.

--- src/components/molecules/Navbar/index.tsx
+++ src/components/molecules/Navbar/index.tsx
@@ -1,7 +1,7 @@
 "use client";
-import React, { useState } from "react";
-import { Button } from "@/components/atoms";
-import { DropdownMenu, NavMenu, Logo } from "@/components/molecules";
+import React, { useEffect, useState } from "react";
+import { Button, Heading } from "@/components/atoms";
+import { DropdownMenu, NavMenu } from "@/components/molecules";
 import { MdClose, MdMenu } from "react-icons/md";
 import { NavigationLink } from "@/types/types";
 
@@ -11,17 +11,53 @@ interface NavbarProps {
 
 export const Navbar: React.FC<NavbarProps> = ({ items }) => {
   const [toggle, setToggle] = useState(false);
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY && currentScrollY > 100) {
+        setShowNavbar(false);
+      } else {
+        setShowNavbar(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
+  const handleCloseMenu = () => {
+    setToggle(false);
+  };
 
   return (
-    <nav className="relative flex items-center justify-between px-8 py-4 bg-popover">
-      <Logo />
+    <nav
+      className={`relative flex items-center justify-between px-8 py-4 bg-popover
+        transform transition-transform duration-300 ${
+          showNavbar ? "translate-y-0" : "-translate-y-full"
+        }
+        `}
+    >
+      <Heading level={3}>JorgeDev</Heading>
       <Button
         onClick={() => setToggle(!toggle)}
         variant="icon"
         icon={toggle ? <MdClose /> : <MdMenu />}
-        className="md:hidden"
+        className="md:hidden border rounded-lg hover:border-primary"
       />
-      <DropdownMenu isOpen={toggle} items={items} />
+      <DropdownMenu
+        isOpen={toggle}
+        items={items}
+        onItemClick={handleCloseMenu}
+      />
       <NavMenu
         items={items}
         className="hidden md:flex md:gap-8"
