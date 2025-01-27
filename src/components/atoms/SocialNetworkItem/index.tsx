355fb3c1c2ff876e14137b12b1810980c332db22
Jamova01 <jorgemova01@gmail.com>
2025-01-26 20:03:16 -0500

    Created SocialNetworkItem component to render a single social network item

---
+++ src/components/atoms/SocialNetworkItem/index.tsx
@@ -0,0 +1,22 @@
+import { SocialNetwork } from "@/types/types";
+import Link from "next/link";
+
+interface SocialNetworkItemProps {
+  network: SocialNetwork;
+}
+
+export const SocialNetworkItem: React.FC<SocialNetworkItemProps> = ({
+  network,
+}) => {
+  return (
+    <li>
+      <Link
+        href="#"
+        className="flex items-center justify-center gap-2 px-2 md:px-4 py-1 md:py-2 border hover:border-primary text-sm lg:text-lg hover:text-primary font-bold rounded-lg"
+      >
+        <span className="text-sm md:lg">{network.icon}</span>
+        {network.name && network.name}
+      </Link>
+    </li>
+  );
+};
