31eec29323ffac4a60e42bf908cb6f0c01cd7731
Jamova01 <jorgemova01@gmail.com>
2025-01-27 09:39:04 -0500

    Added  property to social networks data for dynamic link generation

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -2,11 +2,22 @@ import { Heading, Text } from "@/components/atoms";
 import { SocialNetworks } from "@/components/molecules";
 import { FaInstagram, FaMedium, FaTwitter } from "react-icons/fa";
 import Link from "next/link";
+import { FaX } from "react-icons/fa6";
 
 const socialNetworksData = [
-  { id: 1, icon: <FaInstagram /> },
-  { id: 2, icon: <FaTwitter /> },
-  { id: 3, icon: <FaMedium /> },
+  {
+    id: 1,
+    icon: <FaInstagram />,
+    url: "https://www.instagram.com/jorgemova_dev/",
+  },
+  { id: 2, icon: <FaX />, url: "https://x.com/jorgemova01" },
+  { id: 3, icon: <FaMedium />, url: "https://medium.com/@jorgemova01" },
+];
+
+const navigationLinks = [
+  { id: 1, label: "About", url: "#about" },
+  { id: 2, label: "Projects", url: "#projects" },
+  { id: 3, label: "Contact", url: "#contact" },
 ];
 
 export const Footer = () => {
@@ -18,27 +29,15 @@ export const Footer = () => {
             JorgeDev
           </Heading>
           <ul className="flex gap-4 justify-center">
-            <li>
-              <Link href="#">
-                <Text className="text-sm md:text-lg hover:text-primary">
-                  About
-                </Text>
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <Text className="text-sm md:text-lg hover:text-primary">
-                  Projects
-                </Text>
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <Text className="text-sm md:text-lg hover:text-primary">
-                  Contact
-                </Text>
-              </Link>
-            </li>
+            {navigationLinks.map((link) => (
+              <li key={link.id}>
+                <Link href={link.url}>
+                  <Text className="text-sm md:text-lg hover:text-primary">
+                    {link.label}
+                  </Text>
+                </Link>
+              </li>
+            ))}
           </ul>
           <SocialNetworks data={socialNetworksData} />
         </div>
