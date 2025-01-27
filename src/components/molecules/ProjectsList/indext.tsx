92889eb06d5ada9ece8c07b0674d9c82ccb471d7
Jamova01 <jorgemova01@gmail.com>
2025-01-27 10:58:17 -0500

    add missing data for technologies, GitHub, and deploy URLs in ProjectCard

--- src/components/molecules/ProjectsList/indext.tsx
+++ src/components/molecules/ProjectsList/indext.tsx
@@ -8,15 +8,20 @@ interface Projects {
 export const ProjectsList: React.FC<Projects> = ({ data }) => {
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto">
-      {data.map(({ id, image, title, description }) => (
-        <ProjectCard
-          key={id}
-          id={id}
-          image={image}
-          title={title}
-          description={description}
-        />
-      ))}
+      {data.map(
+        ({ id, image, title, description, technologies, github, deploy }) => (
+          <ProjectCard
+            key={id}
+            id={id}
+            image={image}
+            title={title}
+            description={description}
+            technologies={technologies}
+            github={github}
+            deploy={deploy}
+          />
+        )
+      )}
     </div>
   );
 };
