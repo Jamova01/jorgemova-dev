9f1e1c3835b58c141b5decb43ba1be82a6ca59ac
Jamova01 <jorgemova01@gmail.com>
2025-01-26 19:35:20 -0500

    Add experiences data

---
+++ src/data/experiences_data.ts
@@ -0,0 +1,15 @@
+export const timelineData = [
+  {
+    id: 1,
+    title: "Full Stack Developer",
+    description: `Dirigí el desarrollo de una innovadora solución de gestión de asistentes con Node.js, Express, PostgreSQL y Docker. Implementé la API de OpenAI para automatizar tareas, aumentando la eficiencia operativa de la plataforma en un 30%.`,
+    time: "12/2023 – 04/2024",
+  },
+
+  {
+    id: 2,
+    title: "Frontend Web Developer",
+    description: `Lideré el desarrollo de una plataforma educativa de última generación con Next.js, TypeScript y Tailwind CSS.`,
+    time: "06/2022 – 06/2024",
+  },
+];
